2596fa5b10d9bcff5a58e0ae4418e0c279dd2c4e
Jan Moxter <jan.moxter@moxter.net>
2025-09-10 22:55:01 -0500

    Listener

---
+++ src/main/efaps/ESJP/org/efaps/esjp/loyalty/listener/IOnQuery.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2003 - 2024 The eFaps Team (-)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.efaps.esjp.loyalty.listener;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.admin.program.esjp.IEsjpListener;
+import org.efaps.util.EFapsException;
+
+@EFapsUUID("743babc7-6a54-4628-af31-1683b37de299")
+@EFapsApplication("eFapsApp-Loyalty")
+public interface IOnQuery
+    extends IEsjpListener
+{
+
+    void queryPrograms4Contact(final String identifier)
+        throws EFapsException;
+}
